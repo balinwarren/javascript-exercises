34e93e6bd808a7638f078543a2688dbd9c236395
Balin Warren <balin.warren@gmail.com>
2022-11-05 19:12:41 -0700

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...args) {
+  const arr = args[0];
+  const newArr = [];
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
