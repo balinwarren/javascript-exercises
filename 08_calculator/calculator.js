829ffabd969c745970b4f2421523564f59127e13
Balin Warren <balin.warren@gmail.com>
2022-11-10 16:00:35 -0800

    Completed 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let sum = 0;
+	for (let i = 0; i<args.length; i++){
+    sum += args[i];
+  } return sum
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let tot = 1;
+  for (let i = 0; i<args.length; i++){
+    tot *= args[i];
+  } return tot
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(args) {
+  let tot = 1
+  for (let i = args; i > 0; i--) {
+    tot *= i
+  } return tot;
 };
 
 // Do not edit below this line
